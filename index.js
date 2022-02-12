b81e74ac9c57012897d44acf11adf77c6bce5277
akramHoque <96454691+akramHoque@users.noreply.github.com>
2022-02-12 22:45:49 +0600

    final file added

---
+++ index.js
@@ -0,0 +1,39 @@
+let screen = document.getElementById('screen');
+
+buttons = document.querySelectorAll('button');
+
+let screenValue = '';
+
+for(item of buttons){
+
+      item.addEventListener('click', function(event){
+
+            buttonText = event.target.innerText;
+
+            
+            if(buttonText == 'X'){
+
+                  buttonText = '*'
+
+                  screenValue += buttonText;
+
+                  screen.value = screenValue;
+            }
+            else if(buttonText == 'C'){
+
+                  screenValue = ''
+
+                  screen.value = screenValue
+            }
+            else if(buttonText == '='){
+
+                  screen.value = eval(screenValue)
+            }
+
+            else {
+                screenValue += buttonText;
+
+                screen.value = screenValue  
+            }    
+      })
+}
